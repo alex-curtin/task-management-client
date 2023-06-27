57fc6d9fdcb0cb9f47755b88e960363cc9ad9149
alex.curtin <alex.curtin@marketingevolution.com>
2023-06-27 15:04:46 -0400

    move types to its own directory

---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+export interface ProjectType {
+	id: number;
+	created_at?: string;
+	updated_at?: string;
+	project_name: string;
+	description?: string;
+	created_by?: number | null;
+	owner?: number | null;
+}
+
+export interface ErrorType {
+	message: string;
+}
+
+export interface TaskType {
+	id?: number;
+	task_name: string;
+	description?: string;
+	status?: number;
+	priority?: number;
+	assignee?: number;
+	assignee_username?: string;
+}
+
+export interface UserType {
+	id?: number;
+	username: string;
+	password?: string;
+}
