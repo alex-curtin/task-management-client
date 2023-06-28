522e39b50c4aa385875b96420838ac5a9c3c4f3d
alex.curtin <alex.curtin@marketingevolution.com>
2023-06-27 20:20:17 -0400

    add styling/functionality

--- src/components/add-task/add-task.tsx
+++ src/components/add-task/add-task.tsx
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
 import {
-	Autocomplete,
 	Box,
 	Button,
+	ButtonBase,
 	FormControl,
 	InputLabel,
 	MenuItem,
 	Select,
 	Stack,
 	TextField,
+	Typography,
 } from "@mui/material";
+import { Close } from "@mui/icons-material";
 
-import { useActions } from "../../hooks";
+import { useActions, useSelector } from "../../hooks";
+import { selectLoadingState } from "../../state";
 import { UserType } from "../../types";
 import { fetchAllUsers } from "../../lib";
 import { priorities } from "../../constants";
@@ -31,6 +34,7 @@ export const AddTask: React.FC<AddTaskProps> = ({
 	const [users, setUsers] = useState<UserType[]>([]);
 	const [priority, setPrioriy] = useState<number>(1);
 	const { addProjectTask } = useActions();
+	const { creatingProjectTask } = useSelector(selectLoadingState);
 
 	useEffect(() => {
 		const fetchUsers = async () => {
@@ -41,7 +45,6 @@ export const AddTask: React.FC<AddTaskProps> = ({
 	}, []);
 
 	const handleSubmit = () => {
-		console.log(assignee);
 		const body = {
 			projectId,
 			taskName,
@@ -49,7 +52,6 @@ export const AddTask: React.FC<AddTaskProps> = ({
 			assignee,
 			priority,
 		};
-		console.log("creating task", body);
 		addProjectTask(body);
 		closeModal();
 	};
@@ -62,9 +64,19 @@ export const AddTask: React.FC<AddTaskProps> = ({
 				px: 2,
 				py: 4,
 				borderRadius: 1,
+				maxWidth: "600px",
 			}}
-			spacing={2}
+			spacing={4}
 		>
+			<Box sx={{ display: "flex", justifyContent: "space-between" }}>
+				<Typography sx={{ textTransform: "uppercase" }}>
+					Create A New Task
+				</Typography>
+				<ButtonBase onClick={closeModal}>
+					<Close />
+				</ButtonBase>
+			</Box>
+
 			<TextField
 				label="Task Name"
 				value={taskName}
@@ -75,32 +87,51 @@ export const AddTask: React.FC<AddTaskProps> = ({
 				value={description}
 				onChange={(e) => setDescription(e.target.value)}
 				multiline
-				rows={4}
+				rows={6}
 			/>
-			<FormControl>
-				<InputLabel>Assignee</InputLabel>
-				<Select value={assignee} onChange={(e) => setAssignee(e.target.value)}>
-					{users.map((user) => (
-						<MenuItem key={user.id} value={user.id}>
-							{user.username}
-						</MenuItem>
-					))}
-					<MenuItem value={""} sx={{ display: "none" }} />
-				</Select>
-			</FormControl>
-			<FormControl>
-				<InputLabel>Priority</InputLabel>
-				<Select value={priority} onChange={(e) => setPrioriy(e.target.value)}>
-					{Object.entries(priorities).map(([key, data]) => (
-						<MenuItem key={key} value={parseInt(key)}>
-							{data.label}
-						</MenuItem>
-					))}
-				</Select>
-			</FormControl>
-			<Button variant="contained" onClick={handleSubmit}>
-				Create Task
-			</Button>
+			<Box sx={{ display: "flex", justifyContent: "space-around", gap: 4 }}>
+				<FormControl sx={{ width: "100%" }}>
+					<InputLabel>Assignee</InputLabel>
+					<Select
+						value={assignee}
+						onChange={(e) => setAssignee(e.target.value)}
+					>
+						{users.map((user) => (
+							<MenuItem key={user.id} value={user.id}>
+								{user.username}
+							</MenuItem>
+						))}
+						<MenuItem value={""} sx={{ display: "none" }} />
+					</Select>
+				</FormControl>
+				<FormControl sx={{ width: "100%" }}>
+					<InputLabel>Priority</InputLabel>
+					<Select value={priority} onChange={(e) => setPrioriy(e.target.value)}>
+						{Object.entries(priorities).map(([key, data]) => (
+							<MenuItem key={key} value={parseInt(key)}>
+								{data.label}
+							</MenuItem>
+						))}
+					</Select>
+				</FormControl>
+			</Box>
+
+			<Box sx={{ display: "flex", justifyContent: "flex-end", gap: 1 }}>
+				<Button
+					variant="contained"
+					onClick={handleSubmit}
+					disabled={creatingProjectTask}
+				>
+					Create Task
+				</Button>
+				<Button
+					onClick={closeModal}
+					variant="outlined"
+					disabled={creatingProjectTask}
+				>
+					Cancel
+				</Button>
+			</Box>
 		</Stack>
 	);
 };
