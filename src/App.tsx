16b0de62278b5a4de82b357f0906788990e08c17
alex.curtin <alex.curtin@marketingevolution.com>
2023-06-28 20:05:22 -0400

    add task details component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import { Box } from "@mui/material";
+import { Box, Modal } from "@mui/material";
 
 import { useActions, useSelector } from "./hooks";
 import { HomePage, ProjectPage } from "./pages";
-import { NavBar, Sidebar } from "./components";
-import { selectAuthState } from "./state";
+import { NavBar, Sidebar, TaskDetails } from "./components";
+import { selectAuthState, selectTasksState } from "./state";
 
 const App = () => {
 	const { getCurrentUser } = useActions();
 	const { currentUser } = useSelector(selectAuthState);
+	const { currentTask } = useSelector(selectTasksState);
 
 	useEffect(() => {
 		if (!currentUser) {
@@ -29,6 +30,21 @@ const App = () => {
 					<Route path="/project/:projectId" element={<ProjectPage />} />
 				</Routes>
 			</Box>
+			<Modal open={Boolean(currentTask)}>
+				<Box
+					sx={{
+						background: "white",
+						borderRadius: 1,
+						position: "absolute",
+						top: "50%",
+						left: "50%",
+						transform: "translate(-50%, -50%)",
+						width: "50%",
+					}}
+				>
+					<TaskDetails />
+				</Box>
+			</Modal>
 		</Box>
 	);
 };
---
+++ src/components/task-details/task-details.tsx
@@ -0,0 +1,110 @@
+import { useEffect } from "react";
+import { Box, ButtonBase, Divider, Stack, Typography } from "@mui/material";
+import { Close } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+
+import { useSelector, useActions } from "../../hooks";
+import { selectLoadingState, selectTasksState } from "../../state";
+import { TasksState } from "../../state/reducers/tasksReducer";
+import { TaskDetailsType } from "../../types";
+import { taskStatuses } from "../../constants";
+
+import { PriorityChip } from "../priority-chip";
+
+export const TaskDetails: React.FC = () => {
+	const { currentTask } = useSelector(selectTasksState);
+	const { fetchingCurrentTask } = useSelector(selectLoadingState);
+	const { getCurrentTask, setCurrentTask } = useActions();
+
+	const {
+		assignee,
+		created_by: createdBy,
+		created_at: createdAt,
+		description,
+		id,
+		priority,
+		project_id: projectId,
+		project_name: projectName,
+		status,
+		task_name: taskName,
+	} = currentTask || {};
+
+	const { label: statusLabel } = taskStatuses[status];
+
+	if (fetchingCurrentTask) {
+		return <div>loading</div>;
+	}
+
+	if (!fetchingCurrentTask && !currentTask) {
+		return <div>Can't find that task</div>;
+	}
+
+	return (
+		<Stack sx={{ p: 2 }} spacing={1}>
+			<Box
+				sx={{
+					display: "flex",
+					justifyContent: "space-between",
+					alignItems: "center",
+					mb: 1,
+				}}
+			>
+				<Typography variant="h5">{taskName}</Typography>
+				<ButtonBase onClick={() => setCurrentTask(null)}>
+					<Close />
+				</ButtonBase>
+			</Box>
+
+			<Divider />
+
+			<Box sx={{ minHeight: "150px" }}>
+				<Typography variant="h6">Description</Typography>
+				<Typography>{description}</Typography>
+			</Box>
+			<Divider />
+
+			<Box
+				sx={{
+					display: "flex",
+					justifyContent: "space-between",
+					alignItems: "flex-end",
+				}}
+			>
+				<Box
+					sx={{
+						display: "flex",
+						gap: 1,
+						pb: 1,
+						alignItems: "center",
+					}}
+				>
+					<Link to={`/project/${projectId}`}>
+						<Typography
+							variant="body2"
+							sx={{
+								bgcolor: "grey.600",
+								color: "grey.100",
+								py: "1px",
+								px: "8px",
+								borderRadius: 1,
+							}}
+						>
+							{projectName}
+						</Typography>
+					</Link>
+					<PriorityChip priority={priority} />
+					<Typography>{statusLabel}</Typography>
+				</Box>
+				<Box sx={{ textAlign: "right" }}>
+					<Typography>
+						assigneee: {assignee.username || "unassigned"}
+					</Typography>
+					<Typography>created by: {createdBy.username} </Typography>
+					<Typography>
+						on {new Date(createdAt).toLocaleDateString("en-us")}
+					</Typography>
+				</Box>
+			</Box>
+		</Stack>
+	);
+};
